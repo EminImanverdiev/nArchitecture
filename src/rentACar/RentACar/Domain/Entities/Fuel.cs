bcf8a729c828b12cbdae1a0c58cd6987b1b78779
elmin007 <imanverdiyevemin414@gmail.com>
2025-01-22 20:25:49 +0400

    Added Model,Brand,Fuel entities and etc

---
+++ src/rentACar/RentACar/Domain/Entities/Fuel.cs
@@ -0,0 +1,21 @@
+﻿using Core.Persistence.Repositories;
+using System.Reflection;
+
+namespace Domain.Entities;
+
+public class Fuel : Entity<Guid>
+{
+    public string Name { get; set; }
+    public ICollection<Model> Models{ get; set; }
+    public Fuel()
+    {
+        Models = new List<Model>();
+
+    }
+    public Fuel(Guid id, string name) : this()
+    {
+        Name = Name;
+        Id = id;
+    }
+
+}
---
+++ src/rentACar/RentACar/Domain/Entities/Transmission.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Transmission:Entity<Guid> {
+    public string Name { get; set; }
+    public ICollection<Model> Models { get; set; }
+    public Transmission()
+    {
+        Models = new List<Model>();
+
+    }
+    public Transmission(Guid id, string name) : this()
+    {
+        Name = Name;
+        Id = id;
+    }
+}

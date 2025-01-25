031a3a1c6eea73f00d5a4f6751ed52d89bd7daf3
elmin007 <imanverdiyevemin414@gmail.com>
2025-01-25 22:13:36 +0400

    Added Global Exception to project

---
+++ src/rentACar/RentACar/Application/Features/Brands/Rules/BrandBusinesRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinesRules:BaseBusinessRules
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public BrandBusinesRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+    public async Task BrandNameCannotBeDuplicatedWhenInserted(string name)
+    {
+        Brand? result = await _brandRepository.GetAsync(predicate:b=>b.Name.ToLower()==name.ToLower());
+        if (result != null) {
+            throw new BusinessException(BrandsMessages.BrandNameExists);
+        }
+    }
+}

a32f7a47b922f85d8aebc5768367edaec123ac67
elmin007 <imanverdiyevemin414@gmail.com>
2025-01-28 12:50:02 +0400

    Added distrubed cacing system to project

---
+++ src/corePackages/Core.Packages/Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    public string Cachekey { get;  }
+    public bool BypassCache { get;  }
+    public TimeSpan? SlidingExpiration { get; }
+}
